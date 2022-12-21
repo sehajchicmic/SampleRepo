78d6f67ded443dd18e54a18bcd0400e8d4799bc1
nitesh sharma <nitesh.sharma@chicmic.co.in>
2022-12-21 17:01:48 +0530

    added scroll functionality to card panel

---
+++ Assets/Scripts/Mono_Scripts/MainMenuScene.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuScene : MonoBehaviour
+{
+    public void onClickProfile () {
+        Debug.Log ( "Profile Button clicked!" );
+    }
+    public void onClickAddPack () {
+        Debug.Log ( "Add Packs Button clicked!" );
+    }
+    public void onClickSettings () {
+        Debug.Log ( "Setting Button Clicked!" );
+    }
+    public void onClickShop () {
+        Debug.Log ( "Shop Button Clicked!" );
+    }
+    public void onClickVillage () {
+        Debug.Log ( "Village Button Clicked!" );
+    }
+    public void onClickBingo () {
+        Debug.Log ( "Bingo Button Clicked!" );
+    }
+}
---
+++ Assets/Scripts/Mono_Scripts/scrollViewSystem.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class scrollViewSystem : MonoBehaviour
+{
+    public GameObject scrollbar;
+    bool temp1 = false;
+    bool temp2 = false;
+    bool temp3 = false;
+    bool temp4 = false;
+    private void Update () {
+        if(temp1) {
+            scrollbar.GetComponent<Scrollbar> ().value = Mathf.Lerp ( scrollbar.GetComponent<Scrollbar> ().value, 0.0018f, 0.15f );
+        }
+        if ( temp2 ) {
+            scrollbar.GetComponent<Scrollbar> ().value = Mathf.Lerp ( scrollbar.GetComponent<Scrollbar> ().value, 0.334f, 0.15f );
+        }
+        if ( temp3 ) {
+            scrollbar.GetComponent<Scrollbar> ().value = Mathf.Lerp ( scrollbar.GetComponent<Scrollbar> ().value, 0.6665f, 0.15f );
+        }
+        if ( temp4 ) {
+            scrollbar.GetComponent<Scrollbar> ().value = Mathf.Lerp ( scrollbar.GetComponent<Scrollbar> ().value, 0.9985f, 0.15f );
+        }
+    }
+    public void onClickfirstButton () {
+        temp1 = true;
+        temp2 = false;
+        temp3 = false;
+        temp4 = false;
+    }
+    public void onClickSecondButton () {
+        temp1 = false;
+        temp2 = true;
+        temp3 = false;
+        temp4 = false;
+    }
+    public void onClickThirdButton () {
+        temp1 = false;
+        temp2 = false;
+        temp3 = true;
+        temp4 = false;
+    }
+    public void onClickFourthButton () {
+        temp1 = false;
+        temp2 = false;
+        temp3 = false;
+        temp4 = true;
+    }
+}
