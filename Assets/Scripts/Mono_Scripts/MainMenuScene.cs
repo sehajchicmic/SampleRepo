b3637b3be5c43ff18f25f77534bf1f910e4dced0
nitesh sharma <nitesh.sharma@chicmic.co.in>
2023-01-06 18:40:02 +0530

    onclick function to button panel
    Added: onClick on MainMenuScene , currency add from shop

--- Assets/Scripts/Mono_Scripts/MainMenuScene.cs
+++ Assets/Scripts/Mono_Scripts/MainMenuScene.cs
@@ -1,25 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuScene : MonoBehaviour
 {
+    public GameObject bingoPanel;
+    public GameObject attackPanel;
+    public GameObject shopPanel;
+    public GameObject buttonPanel;
+    public Toggle button;
+    public Toggle button1;
+    public Toggle button2;
+    public Toggle button3;
+    void Start () {
+        bingoPanel.SetActive(true);
+        button1.targetGraphic.color=Color.green;
+        attackPanel.SetActive ( false );
+        shopPanel.SetActive ( false );
+    }
+    #region SETTING PANELS
     public void onClickProfile () {
-        Debug.Log ( "Profile Button clicked!" );
+        //Debug.Log ( "Profile Button clicked!" );
     }
     public void onClickAddPack () {
-        Debug.Log ( "Add Packs Button clicked!" );
+        //Debug.Log ( "Add Packs Button clicked!" );
     }
     public void onClickSettings () {
-        Debug.Log ( "Setting Button Clicked!" );
+        //Debug.Log ( "Setting Button Clicked!" );
     }
     public void onClickShop () {
-        Debug.Log ( "Shop Button Clicked!" );
-    }
-    public void onClickVillage () {
-        Debug.Log ( "Village Button Clicked!" );
+        bingoPanel.SetActive ( false );
+        attackPanel.SetActive ( false );
+        shopPanel.SetActive ( true );
+        //Debug.Log ( "Shop Button Clicked!" );
+        if ( button.isOn ) {
+            button.targetGraphic.color = Color.green;
+        } else {
+            button.targetGraphic.color = Color.white;
+        }
     }
     public void onClickBingo () {
-        Debug.Log ( "Bingo Button Clicked!" );
+        bingoPanel.SetActive ( true );
+        attackPanel.SetActive ( false );
+        shopPanel.SetActive ( false );
+        //Debug.Log ( "Bingo Button Clicked!" );
+        if ( button1.isOn ) {
+            button1.targetGraphic.color = Color.green;
+        } else {
+            button1.targetGraphic.color = Color.white;
+        }
     }
+    public void onClickVillage () {
+        bingoPanel.SetActive ( false );
+        attackPanel.SetActive ( false );
+        shopPanel.SetActive ( false );
+        SceneManager.LoadScene ( "VillageScene" );
+        //Debug.Log ( "Village Button Clicked!" );
+        if ( button2.isOn ) {
+            button2.targetGraphic.color = Color.green;
+        } else {
+            button2.targetGraphic.color = Color.white;
+        }
+    }
+    public void onClickAttack () {
+        bingoPanel.SetActive ( false );
+        attackPanel.SetActive ( true );
+        shopPanel.SetActive ( false );
+        //Debug.Log ( "Attack Button Clicked!" );
+        if ( button3.isOn ) {
+            button3.targetGraphic.color = Color.green;
+        } else {
+            button3.targetGraphic.color = Color.white;
+        }
+    }
+    #endregion
 }
--- Assets/Scripts/Mono_Scripts/shopController.cs
+++ Assets/Scripts/Mono_Scripts/shopController.cs
@@ -4,36 +4,66 @@ using UnityEngine;
 
 public class shopController : MonoBehaviour
 {
+    GameManager gameManager;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = GameManager.instance;
     }
+    #region SHOP CURRENCY
     public void onClickEnergyPanel1 () {
+        gameManager.player.playerEnergy+=1;
+        gameManager.saveLoader.SavePlayerData ( gameManager.player );
+        gameManager.uIManager.SetEnergy ( gameManager.player.playerEnergy );
         Debug.Log ( "1 Energy added ⚡" );
     }
     public void onClickEnergyPanel2 () {
+        gameManager.player.playerEnergy += 5;
+        gameManager.saveLoader.SavePlayerData ( gameManager.player );
+        gameManager.uIManager.SetEnergy ( gameManager.player.playerEnergy );
         Debug.Log ( "5 Energy added ⚡" );
     }
     public void onClickEnergyPanel3 () {
+        gameManager.player.playerEnergy += 20;
+        gameManager.saveLoader.SavePlayerData ( gameManager.player );
+        gameManager.uIManager.SetEnergy ( gameManager.player.playerEnergy );
         Debug.Log ( "20 Energy added ⚡" );
     }
     public void onClickPowerplayPanel1 () {
+        gameManager.player.playerPowers += 5;
+        gameManager.saveLoader.SavePlayerData ( gameManager.player );
+        gameManager.uIManager.SetPowers ( gameManager.player.playerPowers );
         Debug.Log ( "5 Powerplay added" );
     }
     public void onClickPowerplayPanel2 () {
+        gameManager.player.playerPowers += 10;
+        gameManager.saveLoader.SavePlayerData ( gameManager.player );
+        gameManager.uIManager.SetPowers ( gameManager.player.playerPowers );
         Debug.Log ( "10 Powerplay added" );
     }
     public void onClickPowerplayPanel3 () {
+        gameManager.player.playerPowers += 50;
+        gameManager.saveLoader.SavePlayerData ( gameManager.player );
+        gameManager.uIManager.SetPowers ( gameManager.player.playerPowers );
         Debug.Log ( "50 Powerplay added" );
     }
     public void onClickCoinPanel1 () {
+        gameManager.player.playerCoins += 500;
+        gameManager.saveLoader.SavePlayerData ( gameManager.player );
+        gameManager.uIManager.SetCoins ( gameManager.player.playerCoins );
         Debug.Log ( "500 Coin added" );
     }
     public void onClickCoinPanel2 () {
+        gameManager.player.playerCoins += 2000;
+        gameManager.saveLoader.SavePlayerData ( gameManager.player );
+        gameManager.uIManager.SetCoins ( gameManager.player.playerCoins );
         Debug.Log ( "2000 Coin added" );
     }
     public void onClickCoinPanel3 () {
+        gameManager.player.playerCoins += 5000;
+        gameManager.saveLoader.SavePlayerData ( gameManager.player );
+        gameManager.uIManager.SetCoins ( gameManager.player.playerCoins );
         Debug.Log ( "5000 Coin added" );
     }
+    #endregion
 }
